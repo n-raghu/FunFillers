e6caeae2feb5f43235e74f9c03da2f4b68557280
raghu <raghu@LCHVWXVJ2.inspireme.local>
2021-01-20 14:07:17 +0530

    time_bonus

--- guess_words/game.py
+++ guess_words/game.py
@@ -1,11 +1,35 @@
 import sys
+from time import time
 from random import choice, sample
 from operator import itemgetter
 
 from allwords import common_words
 
+
+def map_inputs(letter, guessing, word):
+    if not letter in word:
+        return guessing, False
+    occurences_ = [i for i,x in enumerate(word) if x == letter]
+    for _ in occurences_:
+        guessing[_] = letter
+    return guessing, True
+
+
+def get_suitable_word(all_words=common_words) -> str:
+    while True:
+        _word = choice(common_words()).upper()
+        if len(_word) < 4:
+            continue
+        return _word
+
+
+def calculate_score(_attempts, _len) -> float:
+    score = _len / (_attempts + _len)
+    return round(100 * score, 1)
+
+
 filler = ' __ '
-word: str = choice(common_words()).upper()
+word = get_suitable_word()
 guess = [filler for _ in range(len(word))]
 nchars = int(0.36 * len(word))
 nchars = nchars + 1 if nchars == 1 else nchars
@@ -24,25 +48,20 @@ for l in first_chars:
                 breakpoint = True
 
 incorrect_letters = set()
-
-
-def map_inputs(letter, guessing, word=word):
-    if not letter in word:
-        return guessing, False
-    occurences_ = [i for i,x in enumerate(word) if x == letter]
-    for _ in occurences_:
-        guessing[_] = letter
-    return guessing, True
-
+t = time()
 
 while True:
     if ''.join(guess).strip() == word:
-        print(f'Correct, the word is {word}')
+        total_time = int(time() - t)
+        print(f'Correct, the word is "{word}". ')
+        score = calculate_score(len(incorrect_letters), len(word))
+        time_bonus = 180 - total_time
+        print(f'\nGuess({score}) | TimeBonus({time_bonus})')
         break
     if len(incorrect_letters) > 0:
         print('Miss Fired:', incorrect_letters)
     print(''.join(guess))
     in_char = input('Guess next letter:').upper()
-    guess, hit = map_inputs(in_char, guess)
+    guess, hit = map_inputs(in_char, guess, word)
     if not hit:
         incorrect_letters.add(in_char)
